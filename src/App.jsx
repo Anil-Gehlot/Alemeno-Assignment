3f69e12a997b694fd1469931146b55f5e3053b9f
anil-gehlot <anilgehlot2000@gmail.com>
2025-02-14 14:26:26 +0530

    Entry file for our react app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CourseApp from './CourseApp'
+
+function App() {
+
+  return (
+    <>
+      <CourseApp />
+
+    </>
+  )
+}
+
+export default App
